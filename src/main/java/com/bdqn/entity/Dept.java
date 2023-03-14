901ceabfa4ac868797db13421f8c8d979f48f5d4
扶笙 <1969261597@.com>
2023-03-14 09:26:00 +0800

    hot-fix commit2

--- src/main/java/com/bdqn/entity/Dept.java
+++ src/main/java/com/bdqn/entity/Dept.java
@@ -53,5 +53,6 @@ public class Dept {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
+        System.out.println("hot-fix test!");
     }
 }
